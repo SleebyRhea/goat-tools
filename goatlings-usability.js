2fdb779820690f277dafade39debd500a31bc0ae
Rhea Thorne <17704274+SleebyRhea@users.noreply.github.com>
2024-06-27 22:27:41 -0400

    Bump version

--- goatlings-usability.js
+++ goatlings-usability.js
@@ -10,7 +10,7 @@
 // @grant       GM.getValue
 // @require     https://code.jquery.com/jquery-3.7.1.min.js
 // @license     bsd-3-clause
-// @version     1.0.0
+// @version     1.0.1
 // ==/UserScript==
 
 /* eslint-env jquery */
